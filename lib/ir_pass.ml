f1041e39b49c5290035c3d4d4025f90bfb334ca4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-06-25 23:43:23 +0200

    Add lowering of if statements/blocks

--- lib/ir_pass.ml
+++ lib/ir_pass.ml
@@ -11,19 +11,31 @@ let lower_value expr =
 let lower_basic_expression expr statements = 
     Ok (lower_value expr |> Result.get_ok, statements) (*TODO: Support nested expressions*)
 
-let rec lower_top_block lower_statement lower_expr exprs state =
+let rec lower_block lower_statement lower_expr exprs state =
     Result.bind state (fun state ->
         match exprs with
                 | [] -> Error "Empty program"
                 | [last] -> lower_expr last state |> Result.map (fun (result, statements) -> statements, result)
-                | head :: rest -> lower_statement head state |> lower_top_block lower_statement lower_expr rest)
+                | head :: rest -> lower_statement head state |> lower_block lower_statement lower_expr rest)
 
-let lower_basic_statement statement statements =
+let lower_do_block lower_statement expr statements =
+    match expr with
+        | Tree ((Atom (Identifier "do")) :: nodes) -> lower_block lower_statement lower_basic_expression nodes (Ok statements)
+        | _ -> lower_basic_expression expr statements |> Result.map (fun (a, b) -> b, a)
+
+let rec lower_basic_statement statement statements =
     match statement with
         | Tree [] -> Ok statements
         | Tree ((Atom (Identifier "def")) :: (Atom (Identifier name)) :: expr :: []) -> 
             lower_basic_expression expr statements |> Result.map (fun (value, statements) -> statements @ [ Set (name, value) ])
-        | Tree ((Atom (Identifier "do")) :: _rest) -> failwith "TODO"
+        | Tree ((Atom (Identifier "if")) :: condition :: if_true :: if_false :: []) ->
+            Result.bind (lower_basic_expression condition statements) (fun (l_cond, statements) ->
+                Result.bind (lower_do_block lower_basic_statement if_true statements) (fun l_if_true ->
+                    Result.bind (lower_do_block lower_basic_statement if_false statements) (fun l_if_false ->
+                        Ok (statements @ [ If (l_cond, l_if_true, l_if_false) ])
+                    )
+                )
+            )
         | Atom _ -> Error ("Expected statement, got: " ^ (to_string statement))
         | _ -> Error "not implemented"
 
@@ -32,4 +44,4 @@ let lower_top_statement statement (top_levels, statements) =
         | _ -> lower_basic_statement statement statements |> Result.map (fun sts -> (top_levels, sts))
 
 let lower_program ast = 
-    lower_top_block lower_top_statement (fun expr (sts, _) -> lower_basic_expression expr sts) ast (Ok ([], []))
+    lower_block lower_top_statement (fun expr (sts, _) -> lower_basic_expression expr sts) ast (Ok ([], []))
