3806899cd858187249e3204ba1ec8a57b664b149
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-06-25 23:02:07 +0200

    Prepare for lowering do blocks by creating general function for lowering blocks

--- lib/ir_pass.ml
+++ lib/ir_pass.ml
@@ -11,11 +11,19 @@ let lower_value expr =
 let lower_basic_expression expr statements = 
     Ok (lower_value expr |> Result.get_ok, statements) (*TODO: Support nested expressions*)
 
+let rec lower_block lower_statement exprs state =
+    Result.bind state (fun (top_levels, statements) ->
+        match exprs with
+                | [] -> Error "Empty program"
+                | [last] -> lower_value last |> Result.map (fun result -> let block = statements, result in Ok { top_levels; block })
+                | head :: rest -> lower_statement head top_levels statements |> lower_block lower_statement rest)
+
 let lower_basic_statement statement statements =
     match statement with
         | Tree [] -> Ok statements
         | Tree ((Atom (Identifier "def")) :: (Atom (Identifier name)) :: expr :: []) -> 
             lower_basic_expression expr statements |> Result.map (fun (value, statements) -> statements @ [ Set (name, value) ])
+        | Tree ((Atom (Identifier "do")) :: _rest) -> failwith "TODO"
         | Atom _ -> Error ("Expected statement, got: " ^ (to_string statement))
         | _ -> Error "not implemented"
 
@@ -23,16 +31,5 @@ let lower_top_statement statement top_levels statements =
     match statement with
         | _ -> lower_basic_statement statement statements |> Result.map (fun sts -> (top_levels, sts))
 
-let lower_expr last_state expr =
-    match last_state with
-        | Ok (tl, s) -> lower_top_statement expr tl s
-        | Error e -> Error e
-
 let lower_program ast = 
-    let rec step exprs state =
-        Result.bind state (fun (top_levels, statements) ->
-            match exprs with
-                    | [] -> Error "Empty program"
-                    | [last] -> lower_value last |> Result.map(fun result -> let block = statements, result in Ok { top_levels; block })
-                    | head :: rest -> lower_top_statement head top_levels statements |> step rest)
-    in step ast (Ok ([], []))
+    lower_block lower_top_statement ast (Ok ([], []))
