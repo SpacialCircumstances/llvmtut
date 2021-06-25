463718b3d972c910ab35584f1362b23cf15197b4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-06-25 15:07:42 +0200

    Disallow top-level atoms

--- lib/ir_pass.ml
+++ lib/ir_pass.ml
@@ -1,11 +1,15 @@
+open Ast
 open Ir
 open Result
 
 let lower_basic_expr _expr statements =
     statements
 
-let lower_top_expr _expr _top_levels _statements =
-    Error "unimplemented"
+let lower_top_expr expr top_levels statements =
+    match expr with
+        | Atom _ -> Error ("Error: Atom " ^ (to_string expr) ^ " is not allowed as a top-level statement")
+        | Tree [] -> Ok (top_levels, statements)
+        | _ -> Error "not implemented"
 
 let lower_expr last_state expr =
     match last_state with
