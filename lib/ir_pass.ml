81d759b32a2cf10acee266dba2d874720995eff5
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-06-29 22:03:16 +0200

    Move context out of IR pass module

--- lib/ir_pass.ml
+++ lib/ir_pass.ml
@@ -3,57 +3,6 @@ open Ir
 open Result
 open Containers
 
-module VarSet = CCSet.Make(String)
-
-type defined_function = {
-    arity: int
-}
-
-module DefinedFunctionMap = CCMap.Make(String)
-
-module Context = struct
-    type correct_context = {
-        variables: VarSet.t;
-        functions: defined_function DefinedFunctionMap.t;
-        statements: statement list;
-        top_levels: top_level list;
-    }
-    type t = 
-        | Correct of correct_context
-        | Err of string
-    let empty = Correct {
-        variables = VarSet.empty;
-        functions = DefinedFunctionMap.empty;
-        statements = List.empty;
-        top_levels = List.empty;
-    }
-    let with_error _ctx error = Err error
-    let map apply ctx =
-        match ctx with
-            | Correct ctx -> Correct (apply ctx)
-            | Err e -> Err e
-    let bind apply ctx =
-        match ctx with
-            | Correct ctx -> apply ctx
-            | Err e -> Err e
-    let from_result res = 
-        match res with
-            | Ok ctx -> ctx
-            | Error e -> Err e
-    let add_statement ctx statement = map (fun ctx -> { ctx with statements = ctx.statements @ [statement] }) ctx
-    let add_top_level ctx top_level = map (fun ctx -> { ctx with top_levels = ctx.top_levels @ [top_level] }) ctx
-    let add_variable ctx name = map (fun ctx -> { ctx with variables = VarSet.add name ctx.variables }) ctx
-    let add_function ctx name func = map (fun ctx -> { ctx with functions = DefinedFunctionMap.add name func ctx.functions }) ctx
-    let to_result ctx = match ctx with
-                        | Correct ctx -> Ok ctx
-                        | Err e -> Error e
-    let lookup_variable ctx name = match ctx with
-                                    | Correct ctx -> (match VarSet.mem name ctx.variables with
-                                                        | true -> Ok name
-                                                        | false -> Error ("Variable " ^ name ^ " does not exist"))
-                                    | Err e -> Error e
-end
-
 let lower_value expr ctx =
     match expr with
         | Atom (Identifier id) -> Result.map (fun name -> Variable name) (Context.lookup_variable ctx id), ctx
