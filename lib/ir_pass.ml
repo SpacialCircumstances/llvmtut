a3a73c287361c033868c9f562dc4eb86707335a7
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-06-30 19:52:23 +0200

    Lower function calls

--- lib/ir_pass.ml
+++ lib/ir_pass.ml
@@ -3,26 +3,29 @@ open Ir
 open Result
 open Containers
 
+let swap (a, b) = (b, a)
+
 let lower_value expr ctx =
     match expr with
         | Atom (Identifier id) -> let res, ctx = Context.lookup_variable ctx id in Result.map (fun () -> Variable id) res, ctx
         | Atom (IntLiteral il) -> Ok (Literal (Int64.of_string_exn il)), ctx
         | _ -> let err = "Expected value/atom, got: " ^ (to_string expr) in Error err, (Context.with_error ctx err)
 
-let function_call funcname args ctx =
+let function_call funcname args lower_expr ctx =
     match Context.lookup_function ctx funcname with
         | Ok func, ctx -> if func.arity = List.length args then
-                        Error "not implemented", ctx
+                        let ctx, args_values = List.fold_map (fun ctx arg -> lower_expr arg ctx |> swap) ctx args in
+                        Ok (FunctionCall (funcname, args_values)), ctx
                     else let e = "Function " ^ funcname ^ " has " ^ (Int.to_string func.arity) ^ " arguments, but got " ^ (List.length args |> Int.to_string) in Error e, Context.with_error ctx e
         | Error e, ctx -> Error e, ctx
 
-let lower_expression expr ctx = 
+let rec lower_expression expr ctx = 
     match expr with
         | Atom _ -> let value, ctx = lower_value expr ctx in Result.map (fun v -> Value v) value, ctx
-        | Tree ((Atom (Identifier funcname)) :: args) -> function_call funcname args ctx
+        | Tree ((Atom (Identifier funcname)) :: args) -> function_call funcname args lower_expr_to_value ctx
         | _ -> Error ("Expected expression, got: " ^ (to_string expr)), ctx
 
-let lower_set name expr ctx =
+and lower_set name expr ctx =
     let (expr, ctx) = lower_expression expr ctx in
         match expr with
             | Ok expr ->
@@ -30,7 +33,7 @@ let lower_set name expr ctx =
                 Context.add_statement ctx (Set (name, expr))
             | Error e -> Context.with_error ctx e
 
-let lower_expr_to_value expr ctx = let name, ctx = Context.next_temp ctx in Variable name, lower_set name expr ctx
+and lower_expr_to_value expr ctx = let name, ctx = Context.next_temp ctx in Variable name, lower_set name expr ctx
 
 let rec lower_block lower_statement lower_expr exprs ctx =
     match exprs with
