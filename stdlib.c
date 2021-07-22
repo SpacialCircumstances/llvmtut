ceb4a055dee248feacd1566844ca2e790932e3bf
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-07-22 23:42:05 +0200

    Fix format strings in stdlib

--- stdlib.c
+++ stdlib.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 extern void sl_print(long v) {
-    printf("%i", v);
+    printf("%li", v);
 }
 
 extern long sl_read() {
     long x;
-    if (scanf("%d", &x) != 1) {
+    if (scanf("%li", &x) != 1) {
         exit(32);
     }
     return x;
