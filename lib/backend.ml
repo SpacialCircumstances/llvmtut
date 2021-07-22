bc21781dd04c143083ddad458046a3fe3e8b6814
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-07-22 23:42:13 +0200

    Declare stdlib functions

--- lib/backend.ml
+++ lib/backend.ml
@@ -7,6 +7,7 @@ let context = global_context ()
 let mdl = create_module context "tut"
 let builder = builder context
 let number_type = i64_type context
+let void_type = void_type context
 
 module ValueTable = CCHashtbl.Make(String)
 
@@ -68,6 +69,8 @@ let compile_top_level ctx tl = match tl with
         compile_function ctx fname params statements retval
 
 let generate_native_code irmod = 
+    declare_function "sl_print" (function_type number_type [|void_type|]) mdl |> ignore;
+    declare_function "sl_read" (function_type void_type  [|number_type|]) mdl |> ignore;
     let ctx = { values = ValueTable.create 20 } in
     List.iter (compile_top_level ctx) irmod.top_levels;
     compile_function ctx "main" [] irmod.statements irmod.retval;
